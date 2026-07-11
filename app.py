0467126ab876d07f1243e2ce6055592beb9ec872
Reesey-C <careesejulien@gmail.com>
2026-07-11 18:18:19 -0400

    Added Views

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+
+app = Flask(__name__, template_folder="views")
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/shop")
+def shop():
+    return render_template("shop.html")
+
+
+@app.route("/gallery")
+def gallery():
+    return render_template("gallery.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+@app.route("/book")
+def book():
+    return render_template("book.html", active_page="book")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
